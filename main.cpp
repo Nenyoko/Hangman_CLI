8722e4becc77f2227f1fdcbf10e0cd69ac2929f8
orwenn o <ewennbaudet@yahoo.com>
2021-04-04 14:19:06 +0200

    fix typo

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main(void){
     srand((unsigned int)time(0));
     int selectedword = rand() % wordnumber;
 
-    //choose a world from the list and determine his length.
+    //choose a world from the list and determine its length.
     std::string wordtoguess = wordlist[selectedword];
     int wordtoguess_lenght = wordtoguess.length();
     
@@ -45,12 +45,12 @@ int main(void){
                 playerisright = true;
             }
         }
-        //if the player didn't guess one of the right letters
+        //if the player didn't guess one of the right letters, remove one of his life
         if(playerisright == false){
             playerlife--;
             std::cout << "Wrong letter, try again! Remaining life: " << playerlife << std::endl;
         }
-        //if the player don't have any life, ask the player if he wants to play one more game
+        //if the player don't have any life, ask him if he wants to play one more game
         if(playerlife == 0){
             std::cout << "You lost.. The word was " << wordtoguess << ". Do you want to play again? Y/N" << std::endl;
             std::cin >> userinput;
@@ -70,7 +70,7 @@ int main(void){
                 wordisright = false;
             }
         }
-        //if the word have been guessed, ask the player if he wants to play one more game
+        //if the word have been guessed by the player, ask him if he wants to play one more game
         if(wordisright == true){
             std::cout << wordtoguess_visiblebyplayer << std::endl;
             std::cout << "You won! Do you want to play again? Y/N" << std::endl;
